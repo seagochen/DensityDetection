eec8a35ee04b00bdd14072bca7846847e90c22a5
Orlando Chen <seagochen@hotmail.com>
2023-03-03 00:18:17 +0900

    now script can run in ubuntu 16.04

--- 1.Tools/checksum.py
+++ 1.Tools/checksum.py
@@ -7,31 +7,16 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 # Check if file exists
-file_path = sys.argv[1]
+filepath = sys.argv[1]
 try:
-    with open(file_path, 'rb') as f:
-        pass
-except FileNotFoundError:
+    with open(filepath, 'rb') as f:
+        data = f.read()
+except IOError:
     print("File does not exist")
     sys.exit(1)
 
-# Calculate MD5 hash string
-md5 = hashlib.md5()
-with open(file_path, 'rb') as f:
-    for chunk in iter(lambda: f.read(4096), b""):
-        md5.update(chunk)
-print(f"MD5 hash: {md5.hexdigest()}")
+# Calculate hash string using SHA-256 algorithm
+hash_object = hashlib.sha256(data)
+hash_string = hash_object.hexdigest()
 
-# Calculate SHA1 hash string
-sha1 = hashlib.sha1()
-with open(file_path, 'rb') as f:
-    for chunk in iter(lambda: f.read(4096), b""):
-        sha1.update(chunk)
-print(f"SHA1 hash: {sha1.hexdigest()}")
-
-# Calculate SHA256 hash string
-sha256 = hashlib.sha256()
-with open(file_path, 'rb') as f:
-    for chunk in iter(lambda: f.read(4096), b""):
-        sha256.update(chunk)
-print(f"SHA256 hash: {sha256.hexdigest()}")
+print("SHA-256 hash string for file", filepath, "is", hash_string)
