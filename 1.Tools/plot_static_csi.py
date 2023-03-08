ef3e934c075a2810d7633f8131eecf9de2ad4454
Orlando Chen <seagochen@hotmail.com>
2023-03-08 22:03:14 +0900

    updates for plotting static charts

--- 1.Tools/plot_static_csi.py
+++ 1.Tools/plot_static_csi.py
@@ -1,10 +1,14 @@
-import matplotlib.pyplot as plt
-from wifilib import wifilib as csi
-from wifilib import CSIscale as csi_scale
+import sys
 import numpy as np
+import matplotlib.pyplot as plt
+import csi_scale
+import csi
 
-path = r"./wifilib/run_lh_1.dat"
+if len(sys.argv) < 2:
+    print("Usage: python3 plot_csi.py [csi file]")
+    sys.exit()
 
+path = sys.argv[1]
 bf = csi.read_bf_file(path)
 csi_list = list(map(csi_scale.get_scale_csi,bf))
 csi_np = (np.array(csi_list))
